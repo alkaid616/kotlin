f03eb216c07a664e49ecb39ef2a554b510640e3f
Anastasia Pikalova <anastasia.nekrasova@jetbrains.com>
2024-09-10 23:11:34 +0200

    Make @SubclassOptInRequired accept multiple experimental markers
    [5/5] Add the SubclassOptInRequired test with several markers
    
    ^KT-70562 Fixed

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
@@ -43262,6 +43262,12 @@ public class FirPsiOldFrontendDiagnosticsTestGenerated extends AbstractFirPsiDia
         }
 
         @Test
+        @TestMetadata("SeveralExperimentalMarkers.kt")
+        public void testSeveralExperimentalMarkers() {
+          runTest("compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/SeveralExperimentalMarkers.kt");
+        }
+
+        @Test
         @TestMetadata("Typealias.kt")
         public void testTypealias() {
           runTest("compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/Typealias.kt");
