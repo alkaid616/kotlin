e7afbfb8200602e40f8d75270f2e481c6e211c1c
Brian Norman <brian.norman@jetbrains.com>
2024-09-11 13:46:27 -0500

    fixup! [FIR] Warn when using an intersection type for reified type parameter

--- compiler/testData/diagnostics/tests/inference/reified/ReifiedIntersection.fir.kt
+++ compiler/testData/diagnostics/tests/inference/reified/ReifiedIntersection.fir.kt
@@ -1,14 +1,15 @@
 // ISSUE: KT-52469
 
-interface A
-interface B
+interface InterfaceA
+interface InterfaceB
 
 inline fun <reified T> reifiedType(value: T) {}
+inline fun <reified T> reifiedTypes(vararg values: T) {}
 
 fun <T> testTypeParamter(value: T) {
     when (value) {
-        is A -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
-        is B -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
+        is InterfaceA -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
+        is InterfaceB -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         is String -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         is Int -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         is List<*> -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
@@ -16,12 +17,27 @@ fun <T> testTypeParamter(value: T) {
     }
 }
 
-fun testInterfaces(value: A) {
+fun testInterfaces(value: InterfaceA) {
     when (value) {
-        is B -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
+        is InterfaceB -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         <!USELESS_IS_CHECK!>is String<!> -> <!INFERRED_TYPE_VARIABLE_INTO_POSSIBLE_EMPTY_INTERSECTION, TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         <!USELESS_IS_CHECK!>is Int<!> -> <!INFERRED_TYPE_VARIABLE_INTO_POSSIBLE_EMPTY_INTERSECTION, TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         is List<*> -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedType<!>(value)
         else -> reifiedType(value)
     }
+}
+
+fun testArray(value: InterfaceA) {
+    when (value) {
+        is InterfaceB -> <!INFERRED_TYPE_VARIABLE_INTO_POSSIBLE_EMPTY_INTERSECTION, TYPE_INTERSECTION_AS_REIFIED_WARNING!>Array<!>(1) { _ -> value }
+        <!USELESS_IS_CHECK!>is String<!> -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>Array<!>(1) { _ -> value }
+        <!USELESS_IS_CHECK!>is Int<!> -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>Array<!>(1) { _ -> value }
+        is List<*> -> <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>Array<!>(1) { _ -> value }
+        else -> Array(1) { _ -> value }
+    }
+}
+
+fun testParameters() {
+    <!TYPE_INTERSECTION_AS_REIFIED_WARNING!>reifiedTypes<!>(1, "2", false)
+    reifiedTypes<Any>(1, "2", false)
 }
