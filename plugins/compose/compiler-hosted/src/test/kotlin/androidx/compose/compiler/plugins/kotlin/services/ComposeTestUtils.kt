84b6bf4da5123db5dc50e8b1c18ca5f2ba0a0e19
Jaebaek Seo <jaebaek@google.com>
2024-09-11 11:16:54 -0700

    fixup! K2: Set class FIR isNewPlaceForBodyGeneration on stub-based deserializer

--- plugins/compose/compiler-hosted/src/test/kotlin/androidx/compose/compiler/plugins/kotlin/services/ComposeTestUtils.kt
+++ plugins/compose/compiler-hosted/src/test/kotlin/androidx/compose/compiler/plugins/kotlin/services/ComposeTestUtils.kt
@@ -26,6 +26,11 @@ class ComposePluginAnnotationsProvider(testServices: TestServices) : Environment
         val platform = module.targetPlatform
         when {
             platform.isJvm() -> {
+                /**
+                 * Add all compose UI libraries specified in `RepositoryHandler.composeGoogleMaven()` of
+                 * `repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/ComposeDependencies.kt`
+                 * and `kotlinx` libraries e.g., `kotlinx.coroutines` to JVM class path.
+                 */
                 defaultClassPath.filter { it.absolutePath.contains("androidx.compose") || it.absolutePath.contains("kotlinx") }.forEach {
                     configuration.addJvmClasspathRoot(it)
                 }
